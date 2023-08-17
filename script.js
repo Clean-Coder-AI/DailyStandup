e8bb331d40f35911f29d38664e7162288b4a8641
Swopnil Sapkota <59927837+Swopnil-Sapkota@users.noreply.github.com>
2023-08-17 16:52:09 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const randomizeButton = document.getElementById("randomize-button");
 const notesContainer = document.querySelector(".notes-container");
 const items = sortableList.querySelectorAll(".item");
 
-
 items.forEach(item => {
   item.addEventListener("dragstart", () => {
     setTimeout(() => item.classList.add("dragging"), 0);
@@ -24,54 +23,54 @@ const initSortableList = (e) => {
 sortableList.addEventListener("dragover", initSortableList);
 sortableList.addEventListener("dragenter", e => e.preventDefault());
 
-
 randomizeButton.addEventListener("click", () => {
   const shuffledItems = shuffleArray([...items]);
-  
-  //shuffling
+
+  // Shuffling animation
   items.forEach((item, index) => {
-    item.style.transition = "transform 0.5s";
-    setTimeout(() => {
-      item.style.transition = "";
-    }, 500);
+    item.style.transition = "transform 1s ease-in-out";
+    item.style.transitionDelay = `${index * 0.1}s`; // Add delay to create a staggered effect
     item.style.transform = `translateX(${index * 180}px)`;
   });
 
   setTimeout(() => {
     shuffledItems.forEach((item, index) => {
+      item.style.transition = "transform 1s ease-in-out";
+      item.style.transitionDelay = `${index * 0.1}s`;
       item.style.transform = "";
     });
-  
-    //update names and order in notes container
+
+    // Update names and order in notes container
     notesContainer.style.display = "flex";
     notesContainer.innerHTML = "";
-  
+
     shuffledItems.forEach((item, index) => {
       const personName = item.querySelector(".details span").textContent;
       const personNotesDiv = document.createElement("div");
       personNotesDiv.classList.add("person-notes");
-  
+
       const personNameHeading = document.createElement("h3");
       personNameHeading.classList.add("person-name");
       personNameHeading.textContent = personName;
-  
+
       const notesTextbox = document.createElement("textarea");
       notesTextbox.classList.add("notes-textbox");
       notesTextbox.placeholder = "Write notes here...";
-  
+
       personNotesDiv.appendChild(personNameHeading);
       personNotesDiv.appendChild(notesTextbox);
-  
+
       notesContainer.appendChild(personNotesDiv);
     });
-  }, 1000);
-  
-  //update the order of items after animation...
-  items.forEach((item, index) => {
-    sortableList.appendChild(shuffledItems[index]);
-  });
-});
+  }, items.length * 100 + 1000); // Adjust the delay based on the number of items and animation duration
 
+  // Update the order of items after animation...
+  setTimeout(() => {
+    items.forEach((item, index) => {
+      sortableList.appendChild(shuffledItems[index]);
+    });
+  }, items.length * 100 + 2000); // Adjust the delay based on the number of items and animation duration
+});
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
