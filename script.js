49257f466b868f54f6973ad647f11b4bc740fa0f
Sarwesh Kattel <075bct013.sarwesh@sagarmatha.edu.np>
2023-08-22 14:12:33 +0545

    Stored state in Local Storage

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const saveNotesButton = document.getElementById('saveNotesButton');
     const randomizeButton = document.getElementById('randomizeButton');
 
-    let state = {}; // Store the entire state in this object
+    let noteInputListeners = [];
+
+    let state = {  // Entire state in this object
+        names: [], // Initialize this as an empty array
+        notes: "" // Initialize this as an empty string
+    };
 
     function saveStateToLocalStorage(state) {
         localStorage.setItem('savedState', JSON.stringify(state));
+        // console.log(JSON.stringify(state.names));
+        // console.log(JSON.stringify(state.notes));
+
     }
 
     // Load state from local storage if available
@@ -18,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
         displayState();
     }
 
-    let names = [];
+    // let names = [];
 
     // Initially hide the Save Notes and Randomize List buttons
     saveNotesButton.style.display = 'none';
@@ -26,8 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     generateButton.addEventListener('click', function () {
         names = nameInput.value.split(',').map(name => name.trim());
-        
-
+    
         // Create note containers for each name
         const noteContainers = names.map(name => `
             <div class="note-box">
@@ -35,31 +42,63 @@ document.addEventListener('DOMContentLoaded', function () {
                 <textarea class="note" placeholder="Enter notes for ${name}..."></textarea>
             </div>
         `).join('');
-
+    
+        // // Clear the array and remove previous event listeners
+        // noteInputListeners.forEach(listener => {
+        //     listener.element.removeEventListener('input', listener.handler);
+        // });
+    
+        // noteInputListeners = [];
+    
+        // const noteInputs = notesContainer.querySelectorAll('.note');
+        // noteInputs.forEach((noteInput, index) => {
+        //     const handler = function () {
+        //         state.notes[index] = noteInput.value; // Update the state with the input value
+        //         saveStateToLocalStorage(state); // Save the updated state
+        //     };
+    
+        //     noteInput.addEventListener('input', handler);
+        //     noteInputListeners.push({ element: noteInput, handler });
+        // });
+    
         // Shuffle names and corresponding note containers
-        shuffleArrays(names);
-
+        // shuffleArrays(names);
+    
         const nameList = names.map(name => `<li class="name-box">${name}</li>`).join('');
         nameListContainer.innerHTML = `<ul class="horizontal-list">${nameList}</ul>`;
-
+    
         // Set the note containers HTML content
         notesContainer.innerHTML = noteContainers;
-
+    
         nameListContainer.style.display = 'block';
         notesContainer.style.display = 'block';
-
+    
         // Display the Save Notes and Randomize List buttons after list is generated
         saveNotesButton.style.display = 'block';
         randomizeButton.style.display = 'block';
+    
+        // Update the state object with the current names and notes
+        state.names = names;
+        state.notes = noteContainers;
+    
         saveStateToLocalStorage(state); // Save the state to local storage
+    
         displayState();
     });
-
+    
+    saveNotesButton.addEventListener('click', function () {
+        const notes = Array.from(document.querySelectorAll('.note')).map(noteBox => noteBox.value);
+    
+        // Update the state object with the actual user input notes
+        state.notes = notes;
+    
+        // Save the state to local storage
+        saveStateToLocalStorage(state);
+    });
+             
     randomizeButton.addEventListener('click', function () {
-        // Shuffle names and corresponding note containers
+        
         shuffleArrays(names);
-        shuffleArrays(noteContainers);
-
         updateNameList();
     });
 
@@ -76,7 +115,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function displayState() {
+        console.log(state);
+        console.log("Names property:", JSON.stringify(state.names));
+        console.log("Notes property:", JSON.stringify(state.notes));
         if (state.names) {
+            // console.log("Names property:", JSON.stringify(state.names));
             // Restore the input field value
             nameInput.value = state.names.join(', ');
     
@@ -85,10 +128,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     
         if (state.notes) {
+            // console.log("Notes property:", JSON.stringify(state.notes));
             // Restore the note containers with saved notes
             notesContainer.innerHTML = state.notes;
         }
     }
+
+
     
 
 
